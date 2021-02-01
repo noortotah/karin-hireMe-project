2ccb495bc904d6da638e3663c45e5f5aaeefceba
Noor Totah <noor.totah.91@gmail.com>
2021-02-01 13:47:30 +0200

    add firebase provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { environment } from '../environments/environment';
 
 
 // import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
@@ -42,6 +42,8 @@ import { AngularFireMessagingModule } from '@angular/fire/messaging';
     PopdataPageModule
   ],
   providers: [
+    AngularFireModule,
+    AngularFirestore,
     StatusBar,
     SplashScreen,
     HomePage,
